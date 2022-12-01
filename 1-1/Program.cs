645d27ed1c871e174c031d578bb90ab0e47fa034
DonNinja <yngvileo@gmail.com>
2022-12-01 09:38:52 +0000

    Day 1 problem 1 solved

--- 1-1/Program.cs
+++ 1-1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,52 @@ using System.Threading.Tasks;
 namespace _1_1 {
     internal class Program {
         static void Main(string[] args) {
+            string[] numbers = File.ReadAllLines(args[1]);
 
+            int results = 0;
+
+            switch (args[0]) {
+                case "1":
+                    results = CalculateHighest(numbers);
+                    break;
+                case "2":
+                    CalculateThreeHighest(numbers);
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine($"Calculating highest total as {results}");
+
+            Console.WriteLine("(Press Enter to exit)");
+            Console.ReadLine();
+        }
+
+        static int CalculateHighest(string[] Numbers) {
+            int highestTotal = 0;
+            int currTotal = 0;
+
+            foreach (string num in Numbers) {
+                if (num == "") {
+                    highestTotal = currTotal > highestTotal ? currTotal : highestTotal;
+                    currTotal = 0;
+                }
+                else {
+                    currTotal += int.Parse(num);
+                }
+            }
+
+            return highestTotal;
+        }
+
+        static int CalculateThreeHighest(string[] numbers) {
+            int[] highestInts = { 0, 0, 0 };
+
+            foreach (string num in numbers) {
+
+            }
+
+            return highestInts.Sum();
         }
     }
 }
