1e577ad93b7ca36b5fb882143fd8bcc8f80e8431
DonNinja <yngvileo@gmail.com>
2022-12-04 09:57:20 +0000

    Start day 4

---
+++ Day-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4 {
+    internal class Program {
+        static void Main(string[] args) {
+            string filepath = args[0];
+
+            string[] data = File.ReadAllLines(filepath);
+
+            Console.WriteLine($"The results came in and are: {Part1(data)}");
+        }
+
+        static int Part1(string[] data) {
+            int results = 0;
+
+            int[] numbers = new int[2];
+
+                return results;
+        }
+    }
+}
