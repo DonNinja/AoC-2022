e7b52d6bb40b8de546c48559cff5318b29753478
DonNinja <yngvileo@gmail.com>
2022-12-04 11:53:09 +0000

    Part 1 finished

--- Day-4/Program.cs
+++ Day-4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,12 +14,40 @@ namespace Day_4 {
             string[] data = File.ReadAllLines(filepath);
 
             Console.WriteLine($"The results came in and are: {Part1(data)}");
+
+            Console.WriteLine("(Press enter to quit)");
+            Console.ReadLine();
         }
 
         static int Part1(string[] data) {
             int results = 0;
 
-            int[] numbers = new int[2];
+            foreach (string item in data) {
+                string[] sections = item.Split(',');
+
+                int[] shortSection = new int[2];
+                int[] longSection = new int[2];
+                int minLength = int.MaxValue;
+
+                for (int i = 0; i < sections.Length; i++) {
+                    string[] parts = sections[i].Split('-');
+
+                    int length = int.Parse(parts[1]) - int.Parse(parts[0]);
+                    if (length < minLength) {
+                        minLength = length;
+                        longSection = shortSection;
+                        shortSection = new int[2] { int.Parse(parts[0]), int.Parse(parts[1]) };
+                    }
+                    else {
+                        longSection = new int[2] { int.Parse(parts[0]), int.Parse(parts[1]) };
+                    }
+                }
+
+                if (shortSection[0] >= longSection[0] && shortSection[1] <= longSection[1]) results++;
+            }
+
+            return results;
+        }
 
                 return results;
         }
