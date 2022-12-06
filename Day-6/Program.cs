2b18edbb9b188f42c7ccff8e624109d7bf66cd0d
DonNinja <yngvileo@gmail.com>
2022-12-06 11:12:55 +0000

    Day 6 finished

--- Day-6/Program.cs
+++ Day-6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,34 @@ using System.Threading.Tasks;
 namespace Day_6 {
     internal class Program {
         static void Main(string[] args) {
+            string filepath = args[0];
+
+            string[] data = File.ReadAllLines(filepath);
+
+            //Part1(data);
+            Console.WriteLine($"The results came in and are: {GetPackage(data, 14)}");
+
+            Console.WriteLine("(Press enter to quit)");
+            Console.ReadLine();
+        }
+
+        static int GetPackage(string[] data, int length) {
+            string marker = "";
+            string line = data[0];
+
+            for (int i = 0; i < line.Length; i++) {
+                char letter = line[i];
+                int letterIndex = marker.IndexOf(letter);
+                if (letterIndex != -1) {
+                    marker = marker.Substring(letterIndex + 1);
+                }
+                marker += letter;
+                if (marker.Length == length) {
+                    return i + 1;
+                }
+            }
+
+            return -1;
         }
     }
 }
