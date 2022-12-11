6c2913b3c5558844a69c7c3bfd83e068e054836b
DonNinja <yngvileo@gmail.com>
2022-12-11 11:52:15 +0000

    Started day 7
    Didn't finish

---
+++ Day-7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7 {
+    internal class Program {
+        static void Main(string[] args) {
+            string filepath = args[0];
+
+            string[] data = File.ReadAllLines(filepath);
+
+            //Part1(data);
+            Console.WriteLine($"The results came in and are: {Part1(data)}");
+
+            Console.WriteLine("(Press enter to quit)");
+            Console.ReadLine();
+        }
+
+        static int Part1(string[] data) {
+            int results = 0;
+            foreach (string line in data) {
+                Console.WriteLine(line);
+            }
+
+            return results;
+        }
+    }
+}
